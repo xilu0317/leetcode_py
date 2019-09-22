c414bfea8d81af050243aa52862bbdad193afd2d
Xi Lu <xilu0317@gmail.com>
2019-09-22 15:59:36 -0700

    new

--- course_schedule.py
+++ course_schedule.py
@@ -6,12 +6,12 @@ class Solution(object):
         for pair in prerequisites:
             x, y = pair
             graph[x].append(y)
-        
+
         # visit each node
         for i in range(numCourses):
             if not self.dfsCycleDetected(graph, visited, i):
                 return False
-        
+
         return True
 
     def dfsCycleDetected(self, graph, visited, i):
@@ -21,14 +21,14 @@ class Solution(object):
         # if it is done visted, then do not visit again
         if visited[i] == 1:
             return True
-        
+
         # mark as being visited
         visited[i] = -1
         # visit all the neighbours
         for j in graph[i]:
             if not self.dfsCycleDetected(graph, visited, j):
                 return False
-        
+
         # after visit all the neighbours, mark it as done visited
         visited[i] = 1
         return True
