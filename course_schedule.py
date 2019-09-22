fb80921b6e3884b1a4f8685395e5ec1d9eacfb7b
Xi Lu <xilu0317@gmail.com>
2019-09-22 15:57:24 -0700

    new

--- course_schedule.py
+++ course_schedule.py
@@ -1,20 +1,17 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
         graph = [[] for _ in range(numCourses)]
         visited = [0 for _ in range(numCourses)]
         # create graph
         for pair in prerequisites:
             x, y = pair
             graph[x].append(y)
+        
         # visit each node
         for i in range(numCourses):
             if not self.dfsCycleDetected(graph, visited, i):
                 return False
+        
         return True
 
     def dfsCycleDetected(self, graph, visited, i):
@@ -24,12 +21,14 @@ class Solution(object):
         # if it is done visted, then do not visit again
         if visited[i] == 1:
             return True
+        
         # mark as being visited
         visited[i] = -1
         # visit all the neighbours
         for j in graph[i]:
             if not self.dfsCycleDetected(graph, visited, j):
                 return False
+        
         # after visit all the neighbours, mark it as done visited
         visited[i] = 1
         return True
