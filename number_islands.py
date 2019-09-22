e27aa90a021dbbdfcb331ec4609193bfe4750aae
Xi Lu <xilu0317@gmail.com>
2019-09-22 16:10:30 -0700

    new

--- number_islands.py
+++ number_islands.py
@@ -9,6 +9,7 @@ class Solution:
                 if grid[i][j] == '1':
                     self.dfs(grid, i, j)
                     count += 1
+
         return count
 
     def dfs(self, grid, i, j):
