1dba21d1085733da63a80893f209c1ce56622937
Xi Lu <xilu0317@gmail.com>
2019-09-22 12:57:23 -0700

    new

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums, target):
+        dict = {}
+
+        for i, num in enumerate(nums):
+            if target - num in dict:
+                return ([dict[target - num], i])
+            dict[num] = i
+
+        return([])
