a985f4be0594ee87230165ce56dd6038fc18273e
Xi Lu <xilu0317@gmail.com>
2019-11-11 15:16:57 -0800

    new

--- basics/topological_sort.py
+++ basics/topological_sort.py
@@ -10,25 +10,25 @@ class Graph:
         self.V = vertices  # No. of vertices
 
     # function to add an edge to graph
-    def addEdge(self, u, v):
+    def add_edge(self, u, v):
         self.graph[u].append(v)
 
-    # A recursive function used by topologicalSort
-    def topologicalSortUtil(self, v, visited, stack):
+    # A recursive function used by topological_sort
+    def topological_sort_util(self, v, visited, stack):
         # Mark the current node as visited.
         visited[v] = True
 
         # Recur for all the vertices adjacent to this vertex
         for i in self.graph[v]:
             if visited[i] == False:
-                self.topologicalSortUtil(i, visited, stack)
+                self.topological_sort_util(i, visited, stack)
 
         # Push current vertex to stack which stores result
         stack.insert(0, v)
 
     # The function to do Topological Sort. It uses recursive
-    # topologicalSortUtil()
-    def topologicalSort(self):
+    # topological_sort_util()
+    def topological_sort(self):
         # Mark all the vertices as not visited
         visited = [False] * self.V
         stack = []
@@ -37,20 +37,20 @@ class Graph:
         # Sort starting from all vertices one by one
         for i in range(self.V):
             if visited[i] == False:
-                self.topologicalSortUtil(i, visited, stack)
+                self.topological_sort_util(i, visited, stack)
 
         # Print contents of the stack
         print(stack)
 
 
 g = Graph(6)
-g.addEdge(5, 2)
-g.addEdge(5, 0)
-g.addEdge(4, 0)
-g.addEdge(4, 1)
-g.addEdge(2, 3)
-g.addEdge(3, 1)
+g.add_edge(5, 2)
+g.add_edge(5, 0)
+g.add_edge(4, 0)
+g.add_edge(4, 1)
+g.add_edge(2, 3)
+g.add_edge(3, 1)
 
-print("Following is a Topological Sort of the given graph")
-g.topologicalSort()
+print('Following is a Topological Sort of the given graph')
+g.topological_sort()
 # This code is contributed by Neelam Yadav
