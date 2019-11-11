14b45edfb19b1fad9f33c36c0fb7d8a7e6e10dc7
Xi Lu <xilu0317@gmail.com>
2019-11-11 10:35:41 -0800

    new

---
+++ B_the_rest/basic_calculator.py
@@ -0,0 +1,32 @@
+class Solution:
+    def calculate(self, s):
+        res, num, sign, stack = 0, 0, 1, []
+
+        for c in s:
+            if (c == ' '):
+                continue
+
+            if c.isdigit():
+                num = 10 * num + int(c)
+            elif c == '+':
+                res += sign * num
+                num = 0
+                sign = 1
+            elif c == '-':
+                res += sign * num
+                num = 0
+                sign = -1
+            elif c == '(':
+                stack.append(res)
+                stack.append(sign)
+                sign = 1
+                res = 0
+            elif c == ')':
+                res += sign * num
+                num = 0
+                res *= stack.pop()
+                res += stack.pop()
+
+        res += sign * num
+
+        return res
