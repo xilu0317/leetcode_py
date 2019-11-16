f82d750333dc4c89ace8e732e9b10416413e058a
Xi Lu <xilu0317@gmail.com>
2019-11-16 12:37:15 -0800

    new

---
+++ A_Microsoft/linked_list_cycle.py
@@ -0,0 +1,24 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head):
+        if not head:
+            return False
+
+        slow = fast = head
+
+        while True:
+            if fast.next == None or fast.next.next == None:
+                break
+
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
