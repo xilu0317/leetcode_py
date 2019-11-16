6685aa937397810fead72591c7564d878479e651
Xi Lu <xilu0317@gmail.com>
2019-11-16 12:22:25 -0800

    new

---
+++ python_knowledge/python_class.py
@@ -0,0 +1,20 @@
+class Person(object):
+    # class variable
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def count_people_created(self):
+        return count
+
+    def say_my_name(self):
+        print('name is ' + self.name)
+
+
+p1 = Person('Mason')
+p2 = Person('Xi')
+
+
+Person.count_people_created()
