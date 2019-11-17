2188ae99ae02582971dfa3e38692ff5a4683ea35
Xi Lu <xilu0317@gmail.com>
2019-11-16 17:56:59 -0800

    new

---
+++ A_Microsoft/flatten_tree_linkedlist.py
@@ -0,0 +1,27 @@
+class Solution:
+    def flatten(self, root):
+        if not root:
+            return
+
+        old_left = root.left
+        old_right = root.right
+
+        root.left = None
+
+        self.flatten(old_left)
+        self.flatten(old_right)
+
+        root.right = old_left
+
+        cur = root
+        while cur.right:
+            cur = cur.right
+
+        cur.right = old_right
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
