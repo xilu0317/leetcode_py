379bbef98de88b22a9e61e06833e23571e8b2def
Xi Lu <xi.lu@sophos.com>
2019-11-19 14:27:10 -0800

    new

---
+++ A_Microsoft/merge_k_sorted_list.py
@@ -0,0 +1,29 @@
+from heapq import heappop, heapreplace, heapify
+
+# KEY: tie-breaker: i guarantees uniqueness
+
+
+class Solution:
+    def mergeKLists(self, lists):
+        heap = [(head.val, i, head) for i, head in enumerate(lists) if head]
+        heapify(heap)
+        cur = dummy = ListNode(0)
+
+        while heap:
+            val, i, node = heap[0]
+
+            if node.next:
+                heapreplace(heap, (node.next.val, i, node.next))
+            else:
+                heappop(heap)
+
+            cur.next = node
+            cur = cur.next
+
+        return dummy.next
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
