96564f7b40a511ea0bdb0100ef941a608915e554
Xi Lu <xilu0317@gmail.com>
2019-11-18 19:48:27 -0800

    new

--- A_Microsoft/max_area_islands.py
+++ A_Microsoft/max_area_islands.py
@@ -6,7 +6,6 @@ class Solution:
         def dfs(i, j):
             if 0 <= i < m and 0 <= j < n and grid[i][j]:
                 grid[i][j] = 0
-
                 return 1 + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i + 1, j) + dfs(i, j - 1)
 
             return 0
