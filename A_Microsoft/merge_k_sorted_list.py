77d393a1ce1783b0fa0f528899066657faa7efef
xilu <xilu0317@gmail.com>
2019-11-23 06:39:33 -0800

    test

--- A_Microsoft/merge_k_sorted_list.py
+++ A_Microsoft/merge_k_sorted_list.py
@@ -2,7 +2,7 @@ from heapq import *
 
 
 class Solution:
-    def mergeKLists(self, lists):
+    def merge_k_lists(self, lists):
         heap = [(head.val, i, head) for i, head in enumerate(lists) if head]
         heapify(heap)
         cur = dummy = ListNode(-1)
