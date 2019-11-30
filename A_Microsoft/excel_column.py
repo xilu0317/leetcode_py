de723e43fec9d10252700c1040a0a62e32a983c9
Xi Lu <xilu0317@gmail.com>
2019-11-29 21:06:09 -0600

    new

--- A_Microsoft/excel_column.py
+++ A_Microsoft/excel_column.py
@@ -1,11 +1,13 @@
+from collections import deque
+
+
 class Solution:
-    def convert_to_title(self, num):
-        capitals, res = [chr(x) for x in range(ord('A'), ord('Z') + 1)], []
+    def convertToTitle(self, num):
+        capitals = [chr(x) for x in range(ord('A'), ord('Z') + 1)]
+        res = deque([])
 
         while num:
-            res.append(capitals[(num - 1) % 26])
+            res.appendleft(capitals[(num - 1) % 26])
             num = (num - 1) // 26
-
-        res.reverse()
 
         return ''.join(res)
