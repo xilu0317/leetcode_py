26b193836a1d5cdeb8029cd8c7ee451d5b25a3c3
Xi Lu <xilu0317@gmail.com>
2019-12-02 08:58:47 -0800

    new

---
+++ A_Microsoft/spiral_matrix.py
@@ -0,0 +1,25 @@
+# TODO: BUG
+class Solution:
+    def spiralOrder(self, matrix):
+        row_start, col_start = 0, 0
+        row_end, col_end = len(matrix), len(matrix[0])
+        res = []
+
+        while row_start <= row_end and col_start <= col_end:
+            for x in range(col_start, col_end):
+                res.append(matrix[row_start][x])
+            row_start += 1
+
+            for x in range(row_start, row_end):
+                res.append(matrix[x][col_end])
+            col_end -= 1
+
+            for x in range(col_end, col_start, -1):
+                res.append(matrix[row_end][x])
+            row_end -= 1
+
+            for x in range(row_end, row_start, -1):
+                res.append(matrix[x][col_start])
+            col_start += 1
+
+        return res
