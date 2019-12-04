73b37e8a405ad1fb855a1c2d7a7ed512e823eadb
Xi Lu <xi.lu@sophos.com>
2019-12-04 15:34:29 -0800

    new

--- B_the_rest/string_permutation_1.py
+++ B_the_rest/string_permutation_1.py
@@ -2,15 +2,15 @@ def permute(string):
     return _permute(list(string), 0, len(string) - 1, [])
 
 
-def _permute(a, l, r, res):
+def _permute(arr, l, r, res):
     if l == r:
-        res.append(_to_string(a))
+        res.append(_to_string(arr))
     else:
         for i in range(l, r + 1):
-            a[l], a[i] = a[i], a[l]
-            _permute(a, l + 1, r, res)
+            arr[l], arr[i] = arr[i], arr[l]
+            _permute(arr, l + 1, r, res)
             # backtrack
-            a[l], a[i] = a[i], a[l]
+            arr[l], arr[i] = arr[i], arr[l]
 
     return res
 
