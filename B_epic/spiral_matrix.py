8842869318e09dcbe5dbb95961caf10a933bb5f8
Xi Lu <xilu0317@gmail.com>
2019-12-05 09:07:20 -0800

    new

--- B_epic/spiral_matrix.py
+++ B_epic/spiral_matrix.py
@@ -7,23 +7,29 @@ class Solution:
         row_end, col_end = len(matrix) - 1, len(matrix[0]) - 1
         res = []
 
-        while row_start <= row_end and col_start <= col_end:
+        while True:
             for x in range(col_start, col_end + 1):
                 res.append(matrix[row_start][x])
             row_start += 1
+            if row_start > row_end:
+                break
 
             for x in range(row_start, row_end + 1):
                 res.append(matrix[x][col_end])
             col_end -= 1
+            if col_start > col_end:
+                break
 
-            if row_start <= row_end:
-                for x in range(col_end, col_start - 1, -1):
-                    res.append(matrix[row_end][x])
-                row_end -= 1
+            for x in range(col_end, col_start - 1, -1):
+                res.append(matrix[row_end][x])
+            row_end -= 1
+            if row_start > row_end:
+                break
 
-            if col_start <= col_end:
-                for x in range(row_end, row_start - 1, -1):
-                    res.append(matrix[x][col_start])
-                col_start += 1
+            for x in range(row_end, row_start - 1, -1):
+                res.append(matrix[x][col_start])
+            col_start += 1
+            if col_start > col_end:
+                break
 
         return res
