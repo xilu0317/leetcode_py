d9dc0dd4ff4828ffc2a6f174f95976300e7497a3
Xi Lu <xilu0317@gmail.com>
2019-12-05 08:53:50 -0800

    new

--- B_epic/spiral_matrix.py
+++ B_epic/spiral_matrix.py
@@ -1,28 +1,28 @@
 class Solution:
-    def spiralOrder(self, matrix):
+    def spiral_order(self, matrix):
         if len(matrix) == 0:
             return []
 
         row_start, col_start = 0, 0
-        row_end, col_end = len(matrix), len(matrix[0])
+        row_end, col_end = len(matrix) - 1, len(matrix[0]) - 1
         res = []
 
-        while row_start <= row_end - 1 and col_start <= col_end - 1:
-            for x in range(col_start, col_end):
+        while row_start <= row_end and col_start <= col_end:
+            for x in range(col_start, col_end + 1):
                 res.append(matrix[row_start][x])
             row_start += 1
 
-            for x in range(row_start, row_end):
-                res.append(matrix[x][col_end - 1])
+            for x in range(row_start, row_end + 1):
+                res.append(matrix[x][col_end])
             col_end -= 1
 
-            if row_start <= row_end - 1:
-                for x in range(col_end - 1, col_start - 1, -1):
-                    res.append(matrix[row_end - 1][x])
+            if row_start <= row_end:
+                for x in range(col_end, col_start - 1, -1):
+                    res.append(matrix[row_end][x])
                 row_end -= 1
 
-            if col_start <= col_end - 1:
-                for x in range(row_end - 1, row_start - 1, -1):
+            if col_start <= col_end:
+                for x in range(row_end, row_start - 1, -1):
                     res.append(matrix[x][col_start])
                 col_start += 1
 
