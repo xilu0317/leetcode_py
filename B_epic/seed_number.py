07c0ba262f929732a70dc9df6e6cfa17642402d0
Xi Lu <xilu0317@gmail.com>
2019-12-05 22:28:17 -0800

    new

--- B_epic/seed_number.py
+++ B_epic/seed_number.py
@@ -1,17 +1,8 @@
 # Input: n = 138
 # Output: 23
-# 23 is a seed of 138 because
-# 23*2*3 is equal to 138
 
 # Input: n = 4977
 # Output: 79 711
-# 79 is a seed of 4977 because
-# 79 * 7 * 9 = 4977.
-# 711 is also a seed of 4977 because
-# 711 * 1 * 1 * 7 = 4977
-
-# Input: n = 9
-# Output: No seed exists
 
 # Input: n = 738
 # Output: 123
@@ -35,5 +26,5 @@ def get_seed(num):
     return res if len(res) else None
 
 
-test = get_seed(119)
+test = get_seed(4977)
 print(test)
