e01d5326608dc6ff2334e12e797d080ffa2aeefb
Xi Lu <xi.lu@sophos.com>
2019-12-06 16:40:13 -0800

    new

--- B_the_rest/fib.py
+++ B_the_rest/fib.py
@@ -18,10 +18,10 @@ class Solution2:
         if n == 2:
             return 1
 
-        dp = [0] * (n + 1)
-        dp[1] = dp[2] = 1
+        dp = [0] * n
+        dp[0] = dp[1] = 1
 
-        for i in range(3, n + 1):
+        for i in range(2, n):
             dp[i] = dp[i - 1] + dp[i - 2]
 
-        return dp[n]
+        return dp[n - 1]
