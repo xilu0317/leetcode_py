a145a58e3ce4512c8f04b9fd6a4a7f9409cda1cc
Xi Lu <xilu0317@gmail.com>
2019-12-06 22:09:41 -0800

    new

--- B_epic/colorful_number.py
+++ B_epic/colorful_number.py
@@ -1,2 +1,24 @@
-def colorful_number(n):
-    pass
+def colorful_num(num):
+    num_str = str(num)
+    s = set()
+    for i in range(len(num_str)):
+        for j in range(len(num_str) + 1):
+            if i < j:
+                cur = num_str[i:j]
+                prd = prod(cur)
+                if prd in s:
+                    return False
+                else:
+                    s.add(prd)
+    return True
+
+
+def prod(nums):
+    prd = 1
+    for x in nums:
+        prd *= int(x)
+
+    return prd
+
+
+print(colorful_num(236))
