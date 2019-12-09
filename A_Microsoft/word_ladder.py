c06fbc46d9c71cafe6c314520efd5100a496d3bb
Xi Lu <xi.lu@sophos.com>
2019-12-09 09:54:39 -0800

    new

--- A_Microsoft/word_ladder.py
+++ A_Microsoft/word_ladder.py
@@ -2,11 +2,11 @@ from collections import deque
 
 
 class Solution(object):
-    def ladderLength(self, begin_word, end_word, word_list):
-        word_list = set(word_list)
+    def ladder_length(self, begin_word, end_word, word_list):
+        word_set = set(word_list)
         alphabet = [chr(x) for x in range(ord('a'), ord('z') + 1)]
 
-        q = deque([[begin_word, 1]])
+        q = deque([(begin_word, 1)])
         while q:
             word, length = q.popleft()
             if word == end_word:
@@ -15,8 +15,8 @@ class Solution(object):
             for i in range(len(word)):
                 for c in alphabet:
                     next_word = word[:i] + c + word[i + 1:]
-                    if next_word in word_list:
-                        word_list.remove(next_word)
-                        q.append([next_word, length + 1])
+                    if next_word in word_set:
+                        word_set.remove(next_word)
+                        q.append((next_word, length + 1))
 
         return 0
