29cfd4c3276c6827eb1ba845e8b6337067ec46b4
Xi Lu <xi.lu@sophos.com>
2019-12-10 10:27:36 -0800

    new

---
+++ A_Microsoft/combination_sum.py
@@ -0,0 +1,40 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        res = []
+        self.dfs(candidates, target, 0, [], res)
+
+        return res
+
+    def dfs(self, nums, target, index, path, res):
+        if target < 0:
+            return  # backtracking
+
+        if target == 0:
+            res.append(path)
+            return  # backtracking
+
+        for i in range(index, len(nums)):
+            self.dfs(nums, target - nums[i], i, path + [nums[i]], res)
+
+# faster
+class Solution:
+    def combinationSum(self, candidates, target):
+        result = []
+        candidates = sorted(candidates)
+
+        def dfs(remain, stack):
+            if remain == 0:
+                result.append(stack)
+                return
+
+            for item in candidates:
+                if item > remain:
+                    break
+                if stack and item < stack[-1]:
+                    continue
+                else:
+                    dfs(remain - item, stack + [item])
+
+        dfs(target, [])
+
+        return result
