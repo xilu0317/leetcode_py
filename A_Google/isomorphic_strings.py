ce262c05fb101232a397b54761ec624fe18bc260
Xi Lu <xilu0317@gmail.com>
2019-12-12 21:48:02 -0800

    new

--- A_Google/isomorphic_strings.py
+++ A_Google/isomorphic_strings.py
@@ -1,17 +1,17 @@
 class Solution:
     def is_isomorphic(self, s, t):
-        dic1, dic2 = {}, {}
+        d1, d2 = {}, {}
 
         for i, x in enumerate(s):
-            if x not in dic1:
-                dic1[x] = t[i]
-            elif dic1[x] != t[i]:
+            if x not in d1:
+                d1[x] = t[i]
+            elif d1[x] != t[i]:
                 return False
 
         for i, x in enumerate(t):
-            if x not in dic2:
-                dic2[x] = s[i]
-            elif dic2[x] != s[i]:
+            if x not in d2:
+                d2[x] = s[i]
+            elif d2[x] != s[i]:
                 return False
 
         return True
