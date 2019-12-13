5a5d32b3bc01133604e59b8cfa2d1bfe630a9298
Xi Lu <xilu0317@gmail.com>
2019-12-13 09:19:56 -0800

    new

--- A_Google/license_key_formatting.py
+++ A_Google/license_key_formatting.py
@@ -1,4 +1,21 @@
-# TODO: unfinished
+from collections import deque
+
+
 class Solution:
     def licenseKeyFormatting(self, s, k):
-        pass
+        res, tmp, count = deque([]), deque([]), 0
+        for i, x in enumerate(reversed(s)):
+            if i == len(s) - 1:
+                res.appendleft(''.join(tmp))
+                break
+
+            if x != '-':
+                if count == k:
+                    res.appendleft(''.join(tmp))
+                    tmp.clear()
+                    count = 0
+                else:
+                    tmp.appendleft(x.upper())
+                    count += 1
+
+        return '-'.join(res)
