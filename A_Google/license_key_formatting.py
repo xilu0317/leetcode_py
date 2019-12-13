9437517b02de332c82444f59592e4407aacf9035
Xi Lu <xi.lu@sophos.com>
2019-12-13 13:56:26 -0800

    new

--- A_Google/license_key_formatting.py
+++ A_Google/license_key_formatting.py
@@ -2,20 +2,33 @@ from collections import deque
 
 
 class Solution:
-    def licenseKeyFormatting(self, s, k):
+    def license_key_formatting(self, s, k):
         res, tmp, count = deque([]), deque([]), 0
         for i, x in enumerate(reversed(s)):
-            if i == len(s) - 1:
-                res.appendleft(''.join(tmp))
-                break
-
             if x != '-':
+                tmp.appendleft(x.upper())
+                count += 1
                 if count == k:
                     res.appendleft(''.join(tmp))
                     tmp.clear()
                     count = 0
-                else:
-                    tmp.appendleft(x.upper())
-                    count += 1
+
+        if tmp:
+            res.appendleft(''.join(tmp))
 
         return '-'.join(res)
+
+
+class Solution2:
+    # [BEST]
+    def license_key_formatting(self, s, k):
+        s = s.upper().replace('-', '')
+        n = len(s)
+        s1 = k if n % k == 0 else n % k
+        res = s[:s1]
+
+        while s1 < n:
+            res += '-' + s[s1:s1 + k]
+            s1 += k
+
+        return res
