0f2a4ff32fc7f2acef74a38f706799933f9772c3
Xi Lu <xilu.fullstack@gmail.com>
2019-12-15 08:53:32 -0800

    new

--- A_Microsoft/lowest_common_ancestor_BST.py
+++ A_Microsoft/lowest_common_ancestor_BST.py
@@ -1,9 +1,9 @@
 class Solution:
     def lowest_common_ancestor(self, root, p, q):
-        if root.val > p.val and root.val > q.val:
+        if p.val < root.val and q.val < root.val:
             return self.lowest_common_ancestor(root.left, p, q)
 
-        if root.val < p.val and root.val < q.val:
+        if p.val > root.val and q.val > root.val:
             return self.lowest_common_ancestor(root.right, p, q)
 
         return root
