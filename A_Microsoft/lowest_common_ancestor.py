c65065efbd110b05692262d0ee276931ad3a4b4c
Xi Lu <xilu0317@gmail.com>
2019-12-15 09:20:26 -0800

    new

---
+++ A_Microsoft/lowest_common_ancestor.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lowest_common_ancestor(self, root, p, q):
+        if root is None:
+            return root
+
+        if root is p or root is q:
+            return root
+
+        left = self.lowest_common_ancestor(root.left, p, q)
+        right = self.lowest_common_ancestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left if left else right
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
