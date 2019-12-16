58c508c1718ba82fb2035eb4ecf1546c259c7c71
Xi Lu <xi.lu@sophos.com>
2019-12-16 11:16:04 -0800

    new

--- B_epic/string_permutation_1.py
+++ B_epic/string_permutation_1.py
@@ -4,19 +4,17 @@ def permute(string):
 
 def _permute(arr, l, r, res):
     if l == r:
-        res.append(_to_string(arr))
-    else:
-        for i in range(l, r + 1):
-            arr[l], arr[i] = arr[i], arr[l]
-            _permute(arr, l + 1, r, res)
-            # backtrack
-            arr[l], arr[i] = arr[i], arr[l]
+        res.append(''.join(arr))
+        return None
+
+    for i in range(l, r + 1):
+        arr[l], arr[i] = arr[i], arr[l]
+        _permute(arr, l + 1, r, res)
+        # backtrack
+        arr[l], arr[i] = arr[i], arr[l]
 
     return res
 
 
-def _to_string(list_):
-    return ''.join(list_)
-
-
+print('Print permutation:')
 print(permute('123'))
