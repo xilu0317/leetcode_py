9915b256bb5a48d3141e541d2c3f8d1ad1b5af32
Xi Lu <xilu0317@gmail.com>
2019-12-15 20:06:04 -0800

    new

---
+++ A_Amazon/two_sum_cloest.py
@@ -0,0 +1,15 @@
+def two_sum_closest(nums, target):
+    nums.sort()
+
+    min_ = float('inf')
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        if nums[left] + nums[right] < target:
+            min_ = min(min_, target - nums[left] - nums[right])
+            left += 1
+        else:
+            min_ = min(min_, nums[left] + nums[right] - target)
+            right -= 1
+
+    return min_
