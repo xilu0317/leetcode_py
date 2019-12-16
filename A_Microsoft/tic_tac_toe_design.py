e3c78d55da0a40648b33c7743d4c81118cb7f3af
Xi Lu <xi.lu@sophos.com>
2019-12-16 11:31:32 -0800

    new

--- A_Microsoft/tic_tac_toe_design.py
+++ A_Microsoft/tic_tac_toe_design.py
@@ -4,18 +4,20 @@ class TicTacToe:
         self.diag, self.antidiag = 0, 0
         self.n = n
 
+    # the player places stone on grid (i, j)
     def move(self, i, j, player):
         n = self.n
 
         add_one = 1 if player == 1 else -1
 
         self.rows[i] += add_one
+
         self.cols[j] += add_one
 
         if i == j:
             self.diag += add_one
-        # anti-diagnal
-        if i + j == n - 1:
+
+        if i + j == n - 1:  # anti-diagnal
             self.antidiag = add_one
 
         if abs(self.rows[i]) == n or abs(self.cols[j]) == n or abs(self.diag) == n or abs(self.antidiag) == n:
