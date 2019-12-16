86149b5190e289e41fcbeea20bd1516c17581b4d
Xi Lu <xi.lu@sophos.com>
2019-12-16 10:44:36 -0800

    new

--- B_epic/well_ordered_password.py
+++ B_epic/well_ordered_password.py
@@ -1,6 +1,10 @@
-# num: number to be built
+# num: current number to be built
 # x:   current digit used to construct num
 # k:   counter to keep track of length of num
+
+
+def print_well_ordered(k):
+    _print_well_ordered(0, 0, k)
 
 
 def _print_well_ordered(num, x, k):
@@ -10,10 +14,6 @@ def _print_well_ordered(num, x, k):
 
     for i in range(x + 1, 10):
         _print_well_ordered(num * 10 + i, i, k - 1)
-
-
-def print_well_ordered(k):
-    _print_well_ordered(0, 0, k)
 
 
 print_well_ordered(7)
