892f2e15338aed1a52eb1fc875ea6b0f6a367d98
Xi Lu <xi.lu@sophos.com>
2019-12-17 11:52:32 -0800

    new

--- A_Microsoft/reverse_words_3.py
+++ A_Microsoft/reverse_words_3.py
@@ -2,7 +2,6 @@ class Solution:
     def reverse_words(self, s):
         res = []
         for word in s.split():
-            # word[::-1] reverses the word
             res.append(word[::-1])
 
         return ' '.join(res)
