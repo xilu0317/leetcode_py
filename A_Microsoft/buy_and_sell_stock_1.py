a4f57aa5686612e3bc9d0fd50de4806767b6d913
Xi Lu <xi.lu@sophos.com>
2019-12-17 15:58:05 -0800

    new

--- A_Microsoft/buy_and_sell_stock_1.py
+++ A_Microsoft/buy_and_sell_stock_1.py
@@ -1,9 +1,9 @@
 class Solution:
     def max_profit(self, prices):
-        min_, max_profit = float('inf'), -float('inf')
+        min_, max_ = float('inf'), -float('inf')
 
         for x in prices:
             min_ = min(min_, x)
-            max_profit = max(max_profit, x - min_)
+            max_ = max(max_, x - min_)
 
-        return max_profit if max_profit > 0 else 0
+        return max_ if max_ > 0 else 0
