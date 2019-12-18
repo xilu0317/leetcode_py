fbb616fae88f7e9ede8ebd8ccecd758780678ceb
Xi Lu <xi.lu@sophos.com>
2019-12-17 16:23:40 -0800

    new

--- A_Microsoft/coin_change.py
+++ A_Microsoft/coin_change.py
@@ -3,16 +3,19 @@ class Solution:
     def coin_change(self, coins, amount):
         dp = [0] + [float('inf')] * amount
 
-        for i in range(1, amount + 1):
-            for j in range(len(coins)):
-                if coins[j] <= i:
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+        # try each amount
+        for x in range(1, amount + 1):
+            # check each denomination
+            for i in range(len(coins)):
+                if coins[i] <= x:
+                    # either use or not use the coin
+                    dp[x] = min(dp[x], dp[x - coins[i]] + 1)
 
         return -1 if dp[amount] > amount else dp[amount]
 
 
 class Solution:
-    # [ISSUE] greedy: this ONLY works for nominal coins: 1, 5, 10, 50
+    # [ISSUE] [GREEDY]: this ONLY works for certain denomiation: 1, 5, 10, 50
     def coin_change(self, coins, amount):
         coins.sort()
         count, i = 0, len(coins) - 1
