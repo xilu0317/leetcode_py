dd5679a4f25b705d465aef381843f9bd2db9782f
Xi Lu <xi.lu@sophos.com>
2019-12-17 16:27:11 -0800

    new

--- A_Amazon/two_sum_closest.py
+++ A_Amazon/two_sum_closest.py
@@ -2,15 +2,13 @@
 def two_sum_closest(nums, target):
     nums.sort()
 
-    min_ = float('inf')
-    left, right = 0, len(nums) - 1
+    left, right, min_ = 0, len(nums) - 1, float('inf')
 
     while left < right:
+        min_ = min(min_, abs(target - nums[left] - nums[right]))
         if nums[left] + nums[right] < target:
-            min_ = min(min_, target - nums[left] - nums[right])
             left += 1
         else:
-            min_ = min(min_, nums[left] + nums[right] - target)
             right -= 1
 
     return min_
