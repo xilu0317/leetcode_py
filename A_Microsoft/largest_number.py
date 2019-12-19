bcb741d2cc7720a3171f68f6450e259a49b22a0d
Xi Lu <xi.lu@sophos.com>
2019-12-19 09:47:19 -0800

    new

--- A_Microsoft/largest_number.py
+++ A_Microsoft/largest_number.py
@@ -3,16 +3,16 @@ from functools import cmp_to_key
 
 class Solution:
     def largest_number(self, nums):
-        output = [str(x) for x in sorted(nums, key=cmp_to_key(self.comp))]
+        res = [str(x) for x in sorted(nums, key=cmp_to_key(self.comp), reverse=True)]
 
-        if all(x == '0' for x in output):
+        if all(x == '0' for x in res):
             return '0'
 
-        return ''.join(output)
+        return ''.join(res)
 
     # KEY
     def comp(self, a, b):
         if str(a) + str(b) > str(b) + str(a):
-            return -1
-        else:
             return 1
+        else:
+            return -1
