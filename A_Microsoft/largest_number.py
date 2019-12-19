78db1034425a7c57371cb40fc81f022bf87233b1
xi <xi>
2019-12-18 19:39:00 -0800

    new

---
+++ A_Microsoft/largest_number.py
@@ -0,0 +1,17 @@
+from functools import cmp_to_key
+
+
+class Solution:
+    def largest_number(self, nums):
+        output = [str(x) for x in sorted(nums, key=cmp_to_key(self.comp))]
+
+        if all(x == '0' for x in output):
+            return '0'
+
+        return ''.join(output)
+
+    def comp(self, a, b):
+        if str(a) + str(b) > str(b) + str(a):
+            return -1
+        else:
+            return 1
