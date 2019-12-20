972b696c7fc94d7a7c06a49ff36acae2131e33ab
Xi Lu <xi.lu@sophos.com>
2019-12-20 10:28:52 -0800

    new

--- A_Microsoft/lru_cache.py
+++ A_Microsoft/lru_cache.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, k, v):
-        self.key = k
-        self.val = v
+        self.k = k
+        self.v = v
         self.prev = None
         self.next = None
 
@@ -9,44 +9,44 @@ class Node:
 class LRUCache:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.dic = dict()
+        self.dic = {}
         self.head = Node(0, 0)
         self.tail = Node(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def get(self, key):
-        if key in self.dic:
-            node = self.dic[key]
+    def get(self, k):
+        if k in self.dic:
+            node = self.dic[k]
             self._remove(node)
             self._add(node)
 
-            return node.val
+            return node.v
 
         return -1
 
-    def put(self, key, value):
-        if key in self.dic:
-            self._remove(self.dic[key])
+    def put(self, k, v):
+        if k in self.dic:
+            self._remove(self.dic[k])
 
-        node = Node(key, value)
+        node = Node(k, v)
         self._add(node)
-        self.dic[key] = node
+        self.dic[k] = node
 
         if len(self.dic) > self.capacity:
             node = self.head.next
             self._remove(node)
-            del self.dic[node.key]
+            del self.dic[node.k]
 
     def _remove(self, node):
-        p = node.prev
-        n = node.next
-        p.next = n
-        n.prev = p
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
 
     def _add(self, node):
-        p = self.tail.prev
-        p.next = node
+        prev = self.tail.prev
+        prev.next = node
         self.tail.prev = node
-        node.prev = p
+        node.prev = prev
         node.next = self.tail
