0a18cc3e436dd5bdf106357f0e994dbcf981ce65
xilu0317 <xilu.fullstack@gmail.com>
2025-07-26 15:50:14 -0700

    update

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -2,6 +2,7 @@ class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         dummy = ListNode()
         tail = dummy
+
         while l1 and l2:
             if l1.val < l2.val:
                 tail.next = l1
@@ -10,5 +11,7 @@ class Solution:
                 tail.next = l2
                 l2 = l2.next
             tail = tail.next
+            
         tail.next = l1 or l2
+
         return dummy.next
