b92fa212d3d28de89b673b6f45063056ea175baf
xilu0317 <xilu.fullstack@gmail.com>
2025-07-26 15:51:19 -0700

    update

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -2,10 +2,12 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         pairs = {')': '(', ']': '[', '}': '{'}
+
         for c in s:
             if c in pairs:
                 if not stack or stack.pop() != pairs[c]:
                     return False
             else:
                 stack.append(c)
+                
         return not stack
