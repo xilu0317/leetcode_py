c0434412e8b66ed62828944013fa2447d9c58d1f
xilu0317 <xilu.fullstack@gmail.com>
2025-07-26 15:49:26 -0700

    update

--- merge_intervals.py
+++ merge_intervals.py
@@ -2,18 +2,27 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         # Sort intervals by start time
         intervals.sort(key=lambda x: x[0])
-        merged_intervals = []
+        res = []
 
         for current_interval in intervals:
-            # Get start and end of current interval
-            start, end = current_interval
+            start, end = current_interval # unpacking
             
-            # If merged_intervals is not empty and current interval overlaps
-            if merged_intervals and start <= merged_intervals[-1][1]:
+            if res and start <= res[-1][1]:
                 # Merge by updating the end time
-                merged_intervals[-1][1] = max(merged_intervals[-1][1], end)
+                res[-1][1] = max(res[-1][1], end)
             else:
                 # No overlap, add current interval to result
-                merged_intervals.append(current_interval)
+                res.append(current_interval)
         
-        return merged_intervals
+        return res
+
+
+# unpacking examples
+# Basic unpacking with two values
+numbers = [5, 10]
+start, end = numbers    # start = 5, end = 10
+ 
+# last val example
+arr = [1, 2, 3, 4, 5]
+last = arr[-1]      # 5  (last element)
+second_last = arr[-2]  # 4  (second to last)
