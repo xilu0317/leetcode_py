09c151ac5f59f9cc90bb2af20ac5eb0ead1c78f6
xilu0317 <xilu.fullstack@gmail.com>
2025-07-26 11:04:58 -0700

    update

--- clone_graph.py
+++ clone_graph.py
@@ -1,5 +1,8 @@
 from collections import deque
 
+# The core idea is to create a map and do a bfs traversal on it.
+# The map will store the original node as the key and map to the new node.
+
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
@@ -13,11 +16,13 @@ class Solution:
         while queue:
             curr = queue.popleft()
 
-            for neighbor in curr.neighbors:
-                if neighbor not in dict:
-                    dict[neighbor] = Node(neighbor.val)
-                    queue.append(neighbor)
+            for nb in curr.neighbors:
+                #  Check if the nb has already been visited
+                if nb not in dict:
+                    dict[nb] = Node(nb.val)
+                    # this is needed for the traversal to work
+                    queue.append(nb)
                 
-                dict[curr].neighbors.append(dict[neighbor])
+                dict[curr].neighbors.append(dict[nb])
 
         return dict[node]
