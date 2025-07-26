169d55e846f1a33486119bbce1531bd952066039
xilu0317 <xilu.fullstack@gmail.com>
2025-07-26 15:50:36 -0700

    update

--- search_in_rotated_sorted_array.py
+++ search_in_rotated_sorted_array.py
@@ -1,6 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
+        
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
@@ -15,4 +16,5 @@ class Solution:
                     left = mid + 1
                 else:
                     right = mid - 1
+        
         return -1
