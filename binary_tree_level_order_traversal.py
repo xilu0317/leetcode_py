7f33db775eec2aaba0ac1c4b0b1c1f386fc6be46
xilu0317 <xilu.fullstack@gmail.com>
2025-07-26 15:53:08 -0700

    update

--- binary_tree_level_order_traversal.py
+++ binary_tree_level_order_traversal.py
@@ -1,10 +1,13 @@
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         res = []
+
         if not root:
             return res
         queue = [root]
+        
         while queue:
             res.append([node.val for node in queue])
             queue = [child for node in queue for child in (node.left, node.right) if child]
+        
         return res
